b534b11c3c132b9421a24ce2f968a75f45f156e5
briankc804 <briankc804@gmail.com>
2022-06-15 12:17:16 +0300

    deploying to heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn awwards.wsgi --log-file=-
--- awwards/settings.py
+++ awwards/settings.py
@@ -1,10 +1,10 @@
 import os
 import dj_database_url
-# import django_heroku
+import django_heroku
 from decouple import config, Csv
 
-DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = []
+DEBUG =True
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'fierce-anchorage-60325.herokuapp.com']
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY' )
+SECRET_KEY =('jhjkkj366jwjw' )
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
@@ -68,19 +68,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'awwards.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+ #Database
+ #https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dc96i3qi27si9f',
+        'USER': 'ajvtigugvdarpk',
+    'PASSWORD':'63aafcc122af77f56dd1dc4062c0619f16c248cc7260d2c98a3dc4ae4450e345',
+    'HOST':'ec2-52-71-23-11.compute-1.amazonaws.com',
+    'PORT' : '5432'
+    }
 }
-
 # DATABASE_URL=mysql://myuser:mypassword@myhost/mydatabase
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -136,5 +142,3 @@ REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning'
 }
 
-
-# django_heroku.settings(locals())
